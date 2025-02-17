5dffcac62ed675ebfe2cea5a67f2438345521fa8
pablo <pabloiniestabdn@gmail.com>
2025-02-17 17:32:42 +0100

    Property Entity simplified, favorite feature implemented

---
+++ app/src/main/java/pablo/iniesta/propertylistingchallenge/presentation/propertylist/adapters/PropertyListItemListener.kt
@@ -0,0 +1,8 @@
+package pablo.iniesta.propertylistingchallenge.presentation.propertylist.adapters
+
+import pablo.iniesta.propertylistingchallenge.data.db.PropertyEntity
+
+interface PropertyListItemListener {
+    fun onItemClick(property: PropertyEntity)
+    fun onFavoriteClick(property: PropertyEntity)
+}
